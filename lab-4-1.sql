e2d1cc5eb3d886714ccf3183784dc8497916857a
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-01-10 17:29:00 +0000

    Exercise 4

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,4 +1,11 @@
 -- How many lifetime hits does Barry Bonds have?
+select sum(stats.hits)
+from players
+inner join stats on stats.player_id = players.id
+where players.first_name = "Barry"
+and players.last_name = "Bonds";
+
+-- Foreign key = Primary key
 
 -- Expected result:
 -- 2935
